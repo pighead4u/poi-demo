7210629af98101994b4ba571ef9769608ce04c89
Anjun <u7277205@anu.edu.au>
2022-12-16 16:52:33 +0800

    Added doc strings and comments

--- app/src/main/java/Utils/PythonToPOI.java
+++ app/src/main/java/Utils/PythonToPOI.java
@@ -25,38 +25,55 @@ public class PythonToPOI {
     public static Pattern ENDLOOP_PATTERN = Pattern.compile(ENDLOOP_REGEX);
     public static Pattern MULTI_FIELDS_PATTERN = Pattern.compile(MULTI_FIELDS_REGEX);
 
+    /**
+     * Finds the index of the first valid cell in a row
+     * @param row the row to traverse
+     * @return the index of the first cell; -1 if fails to find
+     */
     public static int findFirstValidCell(XWPFTableRow row) {
-        int res = 0;
+        int res = -1;
         for (int i = 0; i < row.getTableCells().size() - 1; i++) {
             if (row.getTableCells().get(i) != null) {
-                res = i;
-                break;
+                return i;
             }
         }
         return res;
     }
 
+    /**
+     * Finds the index of the first valid paragraph in a cell
+     * @param cell the cell to traverse
+     * @return the index of the first paragraph; -1 if fails to find
+     */
     public static int findFirstValidParagraph(XWPFTableCell cell) {
-        int res = 0;
+        int res = -1;
         for (int i = 0; i < cell.getParagraphs().size() - 1; i++) {
             if (cell.getParagraphs().get(i) != null) {
-                res = i;
-                break;
-            }
-        }
-        return res;
-    }
-    public static int findFirstValidRun(XWPFParagraph para) {
-        int res = 0;
-        for (int i = 0; i < para.getRuns().size() - 1; i++) {
-            if (para.getRuns().get(i) != null) {
-                res = i;
-                break;
+                return i;
             }
         }
         return res;
     }
 
+    /**
+     * Finds the index of the first valid run in a paragraph
+     * @param para the paragraph to traverse
+     * @return the index of the first run; -1 if fails to find
+     */
+    public static int findFirstValidRun(XWPFParagraph para) {
+        int res = -1;
+        for (int i = 0; i < para.getRuns().size() - 1; i++) {
+            if (para.getRuns().get(i) != null) {
+                return i;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Clears all runs in a specific paragraph
+     * @param targetParagraph the paragraph to clear with
+     */
     public static void clearRuns(XWPFParagraph targetParagraph) {
         if (!targetParagraph.isEmpty()) {
             for (int i = targetParagraph.getRuns().size() - 1; i >= 0; i--) {
@@ -65,6 +82,10 @@ public class PythonToPOI {
         }
     }
 
+    /**
+     * Deletes all elements in a row
+     * @param targetRow the row in which to delete
+     */
     public static void clearRow(XWPFTableRow targetRow) {
         if (targetRow.getTableCells().size() != 0) {
             for (int i = targetRow.getTableCells().size() - 1; i >= 0; i--) {
@@ -96,34 +117,38 @@ public class PythonToPOI {
         setTextStyleInRun(newRun);
     }
 
-
-    public static void handles(String filePath) {
-        try (XWPFDocument tempDoc = new XWPFDocument(new FileInputStream(filePath))) {
+    /**
+     * Converts all Python-style elements like for loops, endloop marks, picture fields start with "@", etc. into
+     * POI-tl style elements.
+     * @param inputDir the directory where files to handle with are at
+     * @param outputDir the directory to output the processed files
+     * @param fileName the name of the file to handle with
+     */
+    public static void handles(String inputDir, String outputDir, String fileName) {
+        try (XWPFDocument tempDoc = new XWPFDocument(new FileInputStream(inputDir + fileName))) {
             List<XWPFTable> tables = tempDoc.getTables();
-            // {%tr for item in dts_kgg_jydzsy %}
 
-            int tabAcc = 0;
             for (XWPFTable table : tables) {
-                tabAcc += 1;
                 List<Integer> rowsToDelete = new ArrayList<>();
                 for (int i = 0; i < table.getRows().size(); i++) {
                     XWPFTableRow row = table.getRow(i);
                     for (int j = 0; j < row.getTableCells().size(); j++) {
                         String cellText = row.getCell(j).getText();
+
+                        /* generate corresponding matchers */
                         Matcher loopMatcher = LOOP_PATTERN.matcher(cellText); // match the Python-style for loop
-                        Matcher nationalStdMatcher = NATIONAL_STD_PATTERN.matcher(cellText);
+                        Matcher nationalStdMatcher = NATIONAL_STD_PATTERN.matcher(cellText); // match a
                         Matcher endloopMatcher = ENDLOOP_PATTERN.matcher(cellText);
 
-                        /* If it's the case 检测依据 e.g., section 1.2 */
+                        /* If it's the case 检测依据 e.g., see section 1.2 */
                         if (nationalStdMatcher.find()) {
                             XWPFTableCell cellToPlace = table.getRow(i - 1).getCell(0);
 
-                            XWPFRun cellToPlaceNewRun = cellToPlace.getParagraphs().get(cellToPlace.getParagraphs().size() - 1).createRun();
+                            XWPFRun cellToPlaceNewRun = cellToPlace.getParagraphs().
+                                    get(cellToPlace.getParagraphs().size() - 1).createRun();
                             cellToPlaceNewRun.setText("{" + nationalStdMatcher.group(2) + "}");
-                            cellToPlaceNewRun.setFontSize(FONT_SIZE);
-                            cellToPlaceNewRun.setFontFamily(FONT_FAMILY);
+                            setTextStyleInRun(cellToPlaceNewRun);
                             XWPFTableCell currCell = table.getRow(i).getCell(0);
-
 
                             for (int r = currCell.getParagraphs().size() - 1; r >= 0; r--) {
                                 currCell.removeParagraph(r);
@@ -132,35 +157,35 @@ public class PythonToPOI {
                             currCellNewRun.setText("[" + nationalStdMatcher.group(1) + "]");
                             setTextStyleInRun(currCellNewRun);
                         }
-                        else if (loopMatcher.find()) { // deals with the case if the cell contains the Python-style for loop
+                        else if (loopMatcher.find()) { // deals with the case the cell contains a Python-style for loop
 
                             /* 针对例如"hwg_gtczjhdcl.instrument"与"dts_kgg_jydzsy"两种情况的处理 (有无 ".") */
                             String loopContentToPlace = !loopMatcher.group(2).contains(".") ? loopMatcher.group(2) :
                                     loopMatcher.group(3);
 
-
-                            /* 针对例如 demo.docx 2.22 ~ 2.23 之间的表的情况处理 (匹配到的行为第0行) */
+                            /* 针对例如 demo.docx 2.22 ~ 2.23 之间的表的情况处理 (匹配到的行为第0行)
+                             * 此情况下, 删去原row内容后直接添加新内容*/
                             if (i == 0) {
-                                clearRuns(table.getRow(i).getTableCells().get(0).getParagraphs().get(0));
-                                table.getRow(i).getTableCells().get(0).getParagraphs().get(0).createRun().setText("{" + loopContentToPlace + "}");
-                                setTextStyleInRun(table.getRow(i).getTableCells().get(0).getParagraphs().get(0).getRuns().get(0));
-//                                break;
+                                // TODO: FIX THE ISSUE WHERE THE ROWS AFTER THIS ONE CANNOT BE EDITED
+                                XWPFParagraph paragraph = table.getRow(i).getTableCells().get(0).getParagraphs().get(0);
+                                clearRuns(paragraph);
+                                paragraph.createRun().setText("{" + loopContentToPlace + "}");
+                                setTextStyleInRun(paragraph.getRuns().get(0));
                             }
                             else {
                                 XWPFTableRow prevRow = table.getRow(i - 1);
                                 rowsToDelete.add(i);
                                 XWPFTableCell cellToPlace = prevRow.getCell(0);
 
-                                XWPFRun newRun = cellToPlace.getParagraphs().get(cellToPlace.getParagraphs().size() - 1).
-                                        createRun();
+                                XWPFRun newRun = cellToPlace.getParagraphs().
+                                        get(cellToPlace.getParagraphs().size() - 1).createRun();
                                 newRun.setText("{" + loopContentToPlace + "}");
-                                newRun.setFontFamily("Times New Roman");
-                                newRun.setFontSize(10.5);
+                                setTextStyleInRun(newRun);
                             }
 
                             XWPFTableRow nextRow = table.getRow(i + 1);
 
-                            // get each cell from the next row
+                            /* 通常情况, 将后一行的cell中的原有的字段转化为POI样式的字段 */
                             for (XWPFTableCell cell : nextRow.getTableCells()) {
                                 String nextRowCellText = cell.getText();
                                 Matcher multiFieldsMatcher = MULTI_FIELDS_PATTERN.
@@ -169,19 +194,20 @@ public class PythonToPOI {
                                 if (multiFieldsMatcher.find()) { // 处理表格中一个格子里有一个或多个cell的情况
                                     String multiFieldsMatchGroup_1 = multiFieldsMatcher.group(1);
                                     String cellMatcherResult = (multiFieldsMatchGroup_1 != null
-                                            && multiFieldsMatchGroup_1.equals("@"))
+                                            && multiFieldsMatchGroup_1.equals("@")) // 若有@, 说明该字段为图片且需特殊处理
                                             ? multiFieldsMatchGroup_1 + multiFieldsMatcher.group(3)
                                             : multiFieldsMatcher.group(3);
 
-                                    XWPFParagraph paragraph = cell.getParagraphs().get(findFirstValidParagraph(cell));
-
-                                    ParagraphAlignment paragraphAlignment = paragraph.getAlignment();
+                                    XWPFParagraph paragraph = findFirstValidParagraph(cell) != -1
+                                            ? cell.getParagraphs().get(findFirstValidParagraph(cell))
+                                            : cell.addParagraph();
+                                    ParagraphAlignment paragraphAlignment = paragraph.getAlignment(); // 储存原有格式
 
                                     for (int r = cell.getParagraphs().size() - 1; r >= 0; r--) {
                                         cell.removeParagraph(r);
                                     }
 
-                                    // add new content to cells
+                                    /* 给每个cell添加样式为 [fieldName] 的字段 */
                                     XWPFParagraph newParagraph = cell.addParagraph();
                                     XWPFRun run = newParagraph.createRun();
                                     run.setText("[" + cellMatcherResult + "]");
@@ -190,7 +216,7 @@ public class PythonToPOI {
                                 }
                             }
                         }
-
+                        /* 直接删去row, 若中含有"endloop" */
                         else if (endloopMatcher.find()) {
                             rowsToDelete.add(i); // saves the rows that contain "endloop"
                         }
@@ -199,20 +225,23 @@ public class PythonToPOI {
                 for (int i = rowsToDelete.size() - 1; i >= 0; i--) {
                     table.removeRow(rowsToDelete.get(i));
                 }
-
             }
-
-            FileOutputStream fos = new FileOutputStream("./output/output.docx");
+            /* 将doc写入对应的文件中 */
+            FileOutputStream fos = new FileOutputStream(outputDir + "output_" + fileName);
             tempDoc.write(fos);
         } catch (IOException e) {
             System.out.println("Bad File or Bad File Path");
         }
     }
 
-
     public static void main(String[] args) {
-        String path = "./templates/demo.docx";
-
-        handles(path);
+        File file = new File("./templates/");
+        File[] files = file.listFiles();
+        for (File f : files) {
+            System.out.println("./output/"+f.getName());
+            handles("./templates/", "./output/", f.getName());
+        }
+//        String path = "./templates/demo.docx";
+//        handles(path);
     }
 }
