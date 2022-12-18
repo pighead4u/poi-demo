07f2aa26b122c044ea63c320853401886eb6926a
Anjun <u7277205@anu.edu.au>
2022-12-18 22:56:58 +0800

    Add general optimizations

--- app/src/main/java/Utils/PythonToPOI.java
+++ app/src/main/java/Utils/PythonToPOI.java
@@ -1,6 +1,5 @@
 package Utils;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.File;
@@ -15,15 +14,15 @@ public class PythonToPOI {
     private static final double FONT_SIZE = 10.5;
     private static final String FONT_FAMILY = "Times New Roman";
 
-    public static final String LOOP_REGEX = "%.*for.?(.*)in\\s((.*)\\.|.*).*\\s%"; // {%tr for instrument in hwg_gtczjhdcl.instrument %}
-    public static final String NATIONAL_STD_REGEX = "%.*for\\s(.*)\\sin\\s(.*)\\s%}.*endfor.*";
-    public static final String ENDLOOP_REGEX = "%.*(endfor).*%"; // {%tr endfor %}
-    public static final String MULTI_FIELDS_REGEX = "\\{\\{(@)?(.*)\\.(.*)}}";
+    private static final String LOOP_REGEX = "%.*for.?(.*)in\\s((.*)\\.|.*).*%";
+    private static final String NATIONAL_STD_REGEX = "%.*for\\s(.*)\\sin\\s(.*)\\s%}.*endfor.*";
+    private static final String ENDLOOP_REGEX = "%.*(endfor).*%"; // {%tr endfor %}
+    private static final String MULTI_FIELDS_REGEX = "\\{\\{(@)?(.*)\\.(.*)}}";
 
-    public static Pattern LOOP_PATTERN = Pattern.compile(LOOP_REGEX);
-    public static Pattern NATIONAL_STD_PATTERN = Pattern.compile(NATIONAL_STD_REGEX);
-    public static Pattern ENDLOOP_PATTERN = Pattern.compile(ENDLOOP_REGEX);
-    public static Pattern MULTI_FIELDS_PATTERN = Pattern.compile(MULTI_FIELDS_REGEX);
+    private static final Pattern LOOP_PATTERN = Pattern.compile(LOOP_REGEX);
+    private static final Pattern NATIONAL_STD_PATTERN = Pattern.compile(NATIONAL_STD_REGEX);
+    private static final Pattern ENDLOOP_PATTERN = Pattern.compile(ENDLOOP_REGEX);
+    private static final Pattern MULTI_FIELDS_PATTERN = Pattern.compile(MULTI_FIELDS_REGEX);
 
     /**
      * Finds the index of the first valid cell in a row
@@ -128,16 +127,24 @@ public class PythonToPOI {
         try (XWPFDocument tempDoc = new XWPFDocument(new FileInputStream(inputDir + fileName))) {
             List<XWPFTable> tables = tempDoc.getTables();
 
+            int tabNum = 0;
             for (XWPFTable table : tables) {
+                System.out.println(tabNum);
+                tabNum ++;
                 List<Integer> rowsToDelete = new ArrayList<>();
                 for (int i = 0; i < table.getRows().size(); i++) {
+
+                    System.out.println();
+                    System.out.println();
+                    System.out.println("row number:       " + i);
+
                     XWPFTableRow row = table.getRow(i);
                     for (int j = 0; j < row.getTableCells().size(); j++) {
                         String cellText = row.getCell(j).getText();
-
+                        System.out.println("cell content:         " + cellText);
                         /* generate corresponding matchers */
                         Matcher loopMatcher = LOOP_PATTERN.matcher(cellText); // match the Python-style for loop
-                        Matcher nationalStdMatcher = NATIONAL_STD_PATTERN.matcher(cellText); // match a
+                        Matcher nationalStdMatcher = NATIONAL_STD_PATTERN.matcher(cellText); // match
                         Matcher endloopMatcher = ENDLOOP_PATTERN.matcher(cellText);
 
                         /* If it's the case 检测依据 e.g., see section 1.2 */
@@ -166,7 +173,6 @@ public class PythonToPOI {
                             /* 针对例如 demo.docx 2.22 ~ 2.23 之间的表的情况处理 (匹配到的行为第0行)
                              * 此情况下, 删去原row内容后直接添加新内容*/
                             if (i == 0) {
-                                // TODO: FIX THE ISSUE WHERE THE ROWS AFTER THIS ONE CANNOT BE EDITED
                                 XWPFParagraph paragraph = table.getRow(i).getTableCells().get(0).getParagraphs().get(0);
                                 clearRuns(paragraph);
                                 paragraph.createRun().setText("{" + loopContentToPlace + "}");
@@ -175,17 +181,27 @@ public class PythonToPOI {
                             else {
                                 XWPFTableRow prevRow = table.getRow(i - 1);
                                 rowsToDelete.add(i);
-                                XWPFTableCell cellToPlace = prevRow.getCell(0);
 
-                                XWPFRun newRun = cellToPlace.getParagraphs().
-                                        get(cellToPlace.getParagraphs().size() - 1).createRun();
+                                XWPFTableCell cellToPlace = prevRow.getCell(0);
+                                if (cellToPlace.getText().trim().equals("")) {
+                                    prevRow.removeCell(0);
+                                    cellToPlace = prevRow.getCell(0);
+                                }
+                                System.out.println("-------- cellToPlace is empty?      " + cellToPlace.getText());
+
+                                // TODO: EDIT TO FIX THE "CANNOT ADD CONTENT TO FIRST CELL" ISSUE
+                                XWPFRun newRun = !cellToPlace.getParagraphs().isEmpty()
+                                        ? cellToPlace.getParagraphs().get(cellToPlace.getParagraphs().size() - 1).createRun()
+                                        : cellToPlace.addParagraph().createRun();
                                 newRun.setText("{" + loopContentToPlace + "}");
+                                System.out.println("test:        " + newRun);
+
                                 setTextStyleInRun(newRun);
                             }
 
                             XWPFTableRow nextRow = table.getRow(i + 1);
 
-                            /* 通常情况, 将后一行的cell中的原有的字段转化为POI样式的字段 */
+                            /* 将后一行的cell中的原有的字段转化为POI样式的字段 */
                             for (XWPFTableCell cell : nextRow.getTableCells()) {
                                 String nextRowCellText = cell.getText();
                                 Matcher multiFieldsMatcher = MULTI_FIELDS_PATTERN.
@@ -201,8 +217,6 @@ public class PythonToPOI {
                                     XWPFParagraph paragraph = findFirstValidParagraph(cell) != -1
                                             ? cell.getParagraphs().get(findFirstValidParagraph(cell))
                                             : cell.addParagraph();
-                                    ParagraphAlignment paragraphAlignment = paragraph.getAlignment(); // 储存原有格式
-
                                     for (int r = cell.getParagraphs().size() - 1; r >= 0; r--) {
                                         cell.removeParagraph(r);
                                     }
@@ -212,7 +226,6 @@ public class PythonToPOI {
                                     XWPFRun run = newParagraph.createRun();
                                     run.setText("[" + cellMatcherResult + "]");
                                     setTextStyleInRun(run);
-                                    newParagraph.setAlignment(paragraphAlignment);
                                 }
                             }
                         }
@@ -229,6 +242,8 @@ public class PythonToPOI {
             /* 将doc写入对应的文件中 */
             FileOutputStream fos = new FileOutputStream(outputDir + "output_" + fileName);
             tempDoc.write(fos);
+            tempDoc.close();
+            fos.close();
         } catch (IOException e) {
             System.out.println("Bad File or Bad File Path");
         }
@@ -238,8 +253,13 @@ public class PythonToPOI {
         File file = new File("./templates/");
         File[] files = file.listFiles();
         for (File f : files) {
+            if (f.getName().contains("~$")) {
+                continue;
+            }
             System.out.println("./output/"+f.getName());
-            handles("./templates/", "./output/", f.getName());
+            if (f.getName().equals("高压开关柜.docx")) {
+                handles("./templates/", "./output/", f.getName());
+            }
         }
 //        String path = "./templates/demo.docx";
 //        handles(path);
